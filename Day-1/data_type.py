857c8d74e081716d3e2d98ff5545cc051273ee62
Diki Akbar Asyidiq <diki.akbar1304@gmail.com>
2026-09-10 21:22:41 +0700

    Day 1

---
+++ Day-1/data_type.py
@@ -0,0 +1,39 @@
+
+# String Variable
+string_1 = "Diki"
+
+string_2 = 'Diki 2'
+
+string_3 = """
+Halo
+Ini
+Diki
+"""
+
+string_4 = '''
+Halo
+Ini
+Diki
+'''
+
+# Integer Variable
+integer_1 = 13213213
+
+# Floating Point Variables
+floating_1 = 1.3
+
+# Lists Variable
+lists_1 = [1, "string", 2.3]
+
+# Dictionaries Variable
+dictionaries_1 = {
+    "string" : "string",
+    "integer" : 1,
+    "lists" : [1, "string", 2.3],
+    "dictionaries" : {
+        "key": "value"
+    }
+}
+
+# Boolean Variable
+boolean_1 = True
---
+++ Day-1/expressions.py
@@ -0,0 +1,32 @@
+
+# Sama Dengan
+
+variable_1 = "Diki"
+variable_2 = "Prabuhuy"
+
+print(variable_1 == variable_2)
+
+# Tidak Sama Dengan
+
+variable_1 = "Diki"
+variable_2 = "Prabuhuy"
+
+print(variable_1 != variable_2)
+
+# Perkalian
+
+x = 10
+y = 5
+
+print(x * y)
+
+# Objects Counter
+counter = 0
+lists = [1, 2, 3, 4]
+
+for value in lists:
+    print(f"{counter} + {value} : ")
+    counter += value
+    print(counter)
+    
+print(counter)
